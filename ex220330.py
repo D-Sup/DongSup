3024b70fb7c536480c6b85e135115e93a0e9d1bb
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-03-30 23:41:36 +0900

    연습220330 - 백준(N 찍기, 구구단, 별 찍기)

--- ex220330.py
+++ ex220330.py
@@ -1,11 +1,14 @@
+# 어떤 반에 있는 학생들의 생일이 주어졌을 때, 가장 나이가 적은 사람과 가장 많은 사람을 구하는 프로그램을 작성하시오.
+
 # import sys
 n = int(input())
 info = []
 for _ in range(n):
+    #생년월일 정수형으로 변환 위해 임시로 저장
     info.append(input().split())
     # info.append(list(map(str,input().strip().split())))
     # info.append(input().strip().split())
-#info.sort(key = lambda x: (int(x[1]), int(x[2]), int(x[3])))
-info = sorted(info, key = lambda x: (int(x[1]), int(x[2]), int(x[3])))
+info.sort(key = lambda x: (int(x[1]), int(x[2]), int(x[3])))
+#info = sorted(info, key = lambda x: (int(x[1]), int(x[2]), int(x[3])))
 print(info[0][0])
 print(info[-1][0])
---
+++ ex220330_4.py
@@ -0,0 +1,5 @@
+x = int(input())
+for i in range(1,10) :
+    print(x, i, sep=' * ', end=' = ')
+    print(x*i)
+    #print(x, ' * ', i, ' = ', x*i)

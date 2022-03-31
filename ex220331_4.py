3bde20410f329062e092edf60d55c016d564df24
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-03-31 23:48:18 +0900

    연습220331 - 백준(별찍기, 합, 최대공약수/최소공배수, 영수증)

---
+++ ex220331_4.py
@@ -0,0 +1,7 @@
+m,n=map(int,input().split())
+count=0
+for i in range(1,max(m,n)+1):
+    if m%i==0 and n%i==0:
+        count=i
+print(count)
+print(m//count*n)
---
+++ ex220331_5.py
@@ -0,0 +1,6 @@
+s = int(input())
+for i in range(9):
+    n = int(input())
+    s-=n
+print(s)
+

da70a067159116036954157dc852f5a5890bb198
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-04-02 23:44:25 +0900

    연습220402 - 백준(A+B - 5, 내 학점을 구해줘)

--- ex220402_1.py
+++ ex220402_1.py
@@ -1,8 +1,11 @@
-n = int(input())
 s = []
-for _ in range(n):
+while 1:
     a, b = map(int,input().split())
     s.append(a+b)
+    if s[-1] == 0:
+        del(s[-1])
+        break
 for i in s:
     print(i)
-
+    
+    
---
+++ ex220402_2.py
@@ -0,0 +1,11 @@
+T = int(input())
+C, G = [], []
+for _ in range(T):  
+    N = int(input())
+    C = [0]*T
+    G = [0]*T
+    for _ in range(N):
+        a, b = map(float,input().split())
+        C.append(int(a)),G.append(b)
+    print(sum(C),sum(G)/float(N))
+print()

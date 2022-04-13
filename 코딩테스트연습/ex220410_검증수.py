afda33d8d178271a7110066164484c6e062e1b7d
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-04-13 19:06:14 +0900

    코테테테연습 - 백준(윷놀이)

--- 코딩테스트연습/ex220410_검증수.py
+++ 코딩테스트연습/ex220410_검증수.py
@@ -1,5 +1,15 @@
-N = list(map(int, input().split()))
-R = 0
-for i in N:
-    R += i*i
-print(R % 10)
+# 벡준 2475번
+import sys
+H = 0
+N = list(map(int,sys.stdin.readline().split()))
+for i in range(len(N)):
+    S = N[i]*N[i]
+    H += S
+print(H%10)
+
+
+# N = list(map(int, input().split()))
+# R = 0
+# for i in N:
+#     R += i*i
+# print(R % 10)

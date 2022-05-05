aa5f4019c0be47c017fd29044eba5f554086aef8
tjqehd23 <tjqehd23@naver.com>
2022-05-05 21:58:17 +0900

    코테 - 백준(숫자의 개수)

---
+++ 코딩테스트연습/ex220505_숫자의 개수.py
@@ -0,0 +1,16 @@
+# 백준 2577번 
+import sys
+N = []
+V, r = 1, 0
+for _ in range(3):
+    L = int(sys.stdin.readline())
+    N.append(L)
+for i in range(len(N)):
+    V*=N[i]
+V = list(map(int,str(V)))
+for p in range(10):
+    for q in range(len(V)):
+        if V[q] == p:
+            r+=1
+    print(r)
+    r=0

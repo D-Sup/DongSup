e73b1eda371ff2417467ccc4115f38b22f4a3061
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-05-11 17:31:41 +0900

    코테연습 - 백준(쉽게 푸는 문제)

--- 코딩테스트연습/ex220508_쉽게 푸는 문제.py
+++ 코딩테스트연습/ex220508_쉽게 푸는 문제.py
@@ -1,8 +1,17 @@
 # baekjoon 1292
 import sys
 input = sys.stdin.readline
-l = []
-N, M = map(int,input().split())
-for i in range(N+1,M):
-    l.append(i)
-print(sum(l))
+a, b = map(int,input().split())
+sequence = [0,]
+for i in range(1,100):
+    for _ in range(i):
+        sequence.append(i)
+print(sum(sequence[a:b+1]))
+        
+# import sys
+# input = sys.stdin.readline
+# sequence = [[i]*i for i in range(1,46)]
+# a, b = map(int,input().split())
+# sequence = [0] + sum(sequence,[])
+# print(sum(sequence[a:b+1]))
+# print(sequence[a:b+1])

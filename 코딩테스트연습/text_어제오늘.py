b6ec67b509fe4fab131256b74cee62c67e26d59e
tjqehd23 <tjqehd23@naver.com>
2022-05-13 00:34:13 +0900

    커밋조작용

--- 코딩테스트연습/text_어제오늘.py
+++ 코딩테스트연습/text_어제오늘.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta
+import random
 yesterday = datetime.today() - timedelta(1)
-print(datetime.today().strftime('%A %d %B %Y %H:%M:%S'))
-print(yesterday.strftime('%A %d %B %Y %H:%M:%S'))
+H = random.randrange(20,24)
+M = random.randrange(0,61)
+S = random.randrange(0,61)
+print(datetime.today().strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S)))
+print(yesterday.strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S)))
 
-# git commit --amend --no-edit --date "Sun 3 Oct 2021 12:34:56 KST"
-# GIT_COMMITTER_DATE="Sun 3 Oct 2021 12:34:56 KST" git commit --amend --no-edit
+# git commit --amend --no-edit --date "Thursday 12 May 2022 23:39:21 KST"
+# GIT_COMMITTER_DATE="Thursday 12 May 2022 23:39:21 KST" git commit --amend --no-edit

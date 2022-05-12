c642cb92989d0cd8a9fcab55d96016a41e7ef8b4
tjqehd23 <tjqehd23@naver.com>
2022-05-13 00:40:55 +0900

    커밋변경용

--- 코딩테스트연습/text_어제오늘.py
+++ 코딩테스트연습/text_어제오늘.py
@@ -4,8 +4,11 @@ yesterday = datetime.today() - timedelta(1)
 H = random.randrange(20,24)
 M = random.randrange(0,61)
 S = random.randrange(0,61)
-print(datetime.today().strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S)))
-print(yesterday.strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S)))
-
+dayto = datetime.today().strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S) + ' KST')
+dayyester = yesterday.strftime('%A %d %B %Y ' + str(H)+':'+ str(M) +':'+ str(S) + ' KST')
+print('git commit --amend --no-edit --date "' + dayto + '"' )
+print('GIT_COMMITTER_DATE="' + dayto + '" git commit --amend --no-edit')
+print('git commit --amend --no-edit --date "' + dayyester + '"' )
+print('GIT_COMMITTER_DATE="' + dayyester + '" git commit --amend --no-edit')
 # git commit --amend --no-edit --date "Thursday 12 May 2022 23:39:21 KST"
 # GIT_COMMITTER_DATE="Thursday 12 May 2022 23:39:21 KST" git commit --amend --no-edit

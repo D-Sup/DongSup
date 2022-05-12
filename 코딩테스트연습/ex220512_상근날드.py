6950bf90ad05dcfca75ed4bbed265412285172f7
tjqehd23 <tjqehd23@naver.com>
2022-05-12 23:59:58 +0900

    코테 - 백준(상근날드)

---
+++ 코딩테스트연습/ex220512_상근날드.py
@@ -0,0 +1,11 @@
+# baekjoon 5543
+import sys
+input = sys.stdin.readline
+N, M= [], []
+for _ in range(3):
+    l = int(input())
+    N.append(l)
+for _ in range(2):
+    p = int(input())
+    M.append(p)
+print(min(N)+min(M))

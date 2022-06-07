fbda15fd80af9b91864b9efbbd1991e50880e6c3
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-06-07 14:36:01 +0900

    코테 -프로그래머스(핸드폰 번호 가리기+)

--- 코딩테스트연습/ex220606_핸드폰 번호 가리기.py
+++ 코딩테스트연습/ex220606_핸드폰 번호 가리기.py
@@ -1,6 +1,21 @@
-# programmers 12948
+# # programmers 12948
+# def solution(ans):
+#     for i in range(len(ans[:-4])):
+#         ans[i] = '*'
+#     print(*ans)
+# solution(list(input()))
+
+
+# def solution(ans):
+#     for i in range(len(ans)-4):
+#         ans=ans.replace(ans[i],'*',1)
+#     return ans
+# print(solution('01075675023'))
+
 def solution(ans):
-    for i in range(len(ans[:-4])):
-        ans[i] = '*'
-    print(*ans)
-solution(list(input()))
+    return "*"*(len(ans)-4) + ans[-4:]
+print(solution('01075675023'))
+
+# def solution(ans):
+#     return "*"*len(ans[:-4])+ans[-4:]
+# print(solution('01075675023'))

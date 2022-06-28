4458bb7584f3ad7a1bd8be417426c24a490ce4d4
tjqehd23 <tjqehd23@naver.com>
2022-06-28 11:24:39 +0900

    코테 - 프로그래머스, 백준(소수구하기, 핸드폰 번호 가리기)

--- 코딩테스트연습/ex220603_소수 찾기.py
+++ 코딩테스트연습/ex220603_소수 찾기.py
@@ -10,3 +10,30 @@ for i in M:
         else:
             C += 1
 print(C)
+
+# L = []
+# N = int(input())
+# M = list(map(int,input().split()))
+# for j in M:
+#     if j == 1:
+#         L.append(j)
+#     for i in range(2,j):
+#         if j%i == 0:
+#             L.append(j)
+# L2 = list(set(L))
+# M = [k for k in M if k not in L2]
+# print(len(M))
+
+# n = int(input())
+# nums = list(map(int, input().split(' '))) 
+# resCnt = 0 
+# for i in nums:
+#     cnt = 0 
+#     if(i == 1): 
+#         continue
+#     for j in range(2, i+1):
+#         if(i % j == 0):
+#             cnt += 1
+#     if(cnt == 1):
+#         resCnt += 1
+# print(resCnt)
---
+++ 코딩테스트연습/ex220628_소수구하기.py
@@ -0,0 +1,49 @@
+# baekjoon 1929
+L = []
+a, b = map(int,input().split())
+for i in range(a,b):
+    if i != 1:
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            L.append(i)
+print(*L,sep='\n')
+
+
+# a, b = map(int,input().split())
+# for i in range(a,b):
+#     if i != 1:
+#         for j in range(2,i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             print(i)
+
+
+# x, y = map(int, input().split())
+# for i in range(x, y+1):
+#     print('#',i)
+#     if i == 1: #1은 소수가 아뉘지!
+#         continue
+#     for j in range(2, int(i** 0.5)+1 ):
+#         print(j)
+#         if i%j==0:
+#             break
+#     else:
+#         print(i*'**************************')
+
+
+# m, n = map(int, input().split())
+# def prime_number(x) :
+#   if x == 1 :
+#     return False
+#   else :
+#     for i in range(2, int(x**0.5) + 1) :
+#       if x % i == 0 :
+#         return False
+#     return True
+# for i in range(m, n + 1) :
+#   if prime_number(i) :
+#     print(i)
+

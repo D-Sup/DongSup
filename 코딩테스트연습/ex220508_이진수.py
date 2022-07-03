39fdbbc39d7c1808a744938b49f01e52556c2a55
tjqehd23 <tjqehd23@naver.com>
2022-07-03 21:13:08 +0900

    코테 - 백준(소수)

--- 코딩테스트연습/ex220508_이진수.py
+++ 코딩테스트연습/ex220508_이진수.py
@@ -1,21 +1,24 @@
 # baekjoon 3460
 import sys
 input = sys.stdin.readline
-N = int(input())
-M = []
-while 0 < N:
-    K = N % 2 
-    M.append(K)
-    N = N // 2
-for i in range(len(M)):
-    if M[i] == 1:
-        print(i, end=' ')
-print(M)
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    M = []
+    while 0 < N:
+        K = N % 2 
+        M.append(K)
+        N = N // 2
+    for i in range(len(M)):
+        if M[i] == 1:
+            print(i, end=' ')
+    print()
 
 # T = int(input())
-
 # for _ in range(T):
 #     n = bin(int(input()))[2:]
+#     print(n)
 #     for i in range(len(n)):
+#         print('-----',n[-i-1])
 #         if n[-i - 1] == '1':
 #             print(i, end=' ')

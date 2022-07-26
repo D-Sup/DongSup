6982c16e513b2b6103cc0845d692e695390f0a82
tjqehd23 <tjqehd23@naver.com>
2022-07-26 20:18:58 +0900

    이코테 - 문자열을 정수로 바꾸기

---
+++ 코딩테스트연습/ex220726_문자열을 정수로 바꾸기 .py
@@ -0,0 +1,27 @@
+# 문자열을 정수로 바꾸기 
+L = []
+def solution(s):
+    for i in str(s):
+        if i.isdigit():
+            L.append(int(i))
+        else:
+            if i == "-":
+                L.append(i)
+    return L
+print(*solution("-1234"),sep='')
+
+
+# def solution(s):
+#     return s
+# print("-1234")
+
+
+# def strToInt(str):
+#     result = 0
+#     for idx, number in enumerate(str[::-1]):
+#         if number == '-':
+#             result *= -1
+#         else:
+#             result += int(number) * (10 ** idx)
+#     return result
+# print(strToInt("-1234"));

460e9d6c1b8cdc05d8d6ed1d253e240adcefed7c
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-08-05 15:07:21 +0900

    test

--- 코딩테스트연습/ex220530_x만큼 간격이 있는 n개의 숫자.py
+++ 코딩테스트연습/ex220530_x만큼 간격이 있는 n개의 숫자.py
@@ -2,3 +2,10 @@
 def solution(x, n):
     return [x*i for i in range(1,n+1)] 
 print(solution(2,5))
+
+# new method
+def solution(x, n):
+    answer = [a for a in range(x, (n+1)*x, x)] if x else [0]*n
+    # = [i*x*x for i in range(n)]
+    return answer
+print(solution(2,5))

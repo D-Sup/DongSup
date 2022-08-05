3bbcdc50bc691378a4ac358898b2f7485b64e74e
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-08-05 16:54:13 +0900

    코테 - 프로그래머스(서울에서 김서방 찾기)

---
+++ 코딩테스트연습/ex220805_ 서울에서 김서방 찾기.py
@@ -0,0 +1,12 @@
+# programmers 12919
+def solution(seoul):
+    N = seoul.index("Kim")
+    return f'김서방은 {N}에 있다'
+    
+print(solution(["Jane", "Kim"]))
+
+# def solution(seoul):
+#     return "김서방은 {}에 있다".format(seoul.index('Kim'))
+# print(solution(["Jane", "Kim"]))
+
+

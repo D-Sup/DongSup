cd36286ce0f21d20beda588d7b5a069b3903baaf
tjqehd23 <tjqehd23@naver.com>
2022-08-08 22:50:19 +0900

    코테 - 프로그래머스(문자열 다루기)

--- 코딩테스트연습/ex220807_소수 찾기.py
+++ 코딩테스트연습/ex220807_소수 찾기.py
@@ -1,10 +1,11 @@
 # programmers 12921
 def solution(n):
-    num = set(range(2,n+1))
+    num = set(range(2,n+1)) 
 
-    for i in range(2,n+1):
-        num -= set(range(i*2,n+1,i))
-    
+    for i in range(2,n+1): 
+        print(i)
+        num -= set(range(i*2,n+1,i)) 
+        print(num)
     return len(num)
 
 print(solution(10))
@@ -21,3 +22,19 @@ print(solution(10))
 #         if solution(i):
 #             r+=1
 #     print(r)
+
+# ==================================================
+# def solution(n):
+#     return len([i for i in range(2, n + 1) if len([True for j in range(1, i) if i % j == 0]) == True])
+# print(solution(10))
+
+# r = 0
+# for i in range(2, 10 + 1):
+#     L = []
+#     for j in range(1, i):
+#         if i % j == 0:
+#             L.append(True)
+#     if len(L) == 1:
+#         r += 1
+# print(r)
+# ==================================================
---
+++ 코딩테스트연습/ex220808_문자열 다루기.py
@@ -0,0 +1,12 @@
+# programmers 12918
+def solution(s):
+    return s.isdigit() and len(s) in (4,6)
+print(solution("12a4"))
+
+# def solution(s):
+#     if (len(s) == 4 or len(s) == 6) and s.isdigit():
+#         return True
+#     else:
+#         return False
+# print(solution("1234"))
+

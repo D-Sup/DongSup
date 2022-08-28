6de6d50f70696f3f4fb09ebcb5a0d813c8e638dd
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-08-28 23:14:24 +0900

    코테테 - 프로그래머스(신고 결과 받기)

--- 코딩테스트연습/ex220820_성격 유형 검사하기.py
+++ 코딩테스트연습/ex220820_성격 유형 검사하기.py
@@ -21,3 +21,25 @@ def solution(survey,choices):
     return R
 print(solution(["TR", "RT", "TR"],[7,1,3]))
 
+
+from collections import defaultdict
+
+def solution(survey, choices):
+    scores = defaultdict(int) 
+    for (first, second), choice in zip(survey, choices):
+        survey_choice = first if choice < 4 else second # 고른 선택지의 번호에 따라 유형의 알파벳이 무엇인지 
+        scores[survey_choice] += abs(4 - choice) # 그 알파벳에 대한 점수를 합산 
+
+    answer = ""
+    for first, second in ("RT", "CF", "JM", "AN"): # scores = defaultdict(int)  점수가 아예 없는 유형은 자동으로 디폴트값이 0이 되도록 
+        answer += first if scores[second] < scores[first] \
+            else second if scores[first] < scores[second] \
+            else min(first, second)
+    return answer
+
+if __name__ == '__main__':
+    result = solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
+    print("TCMA" == result, result)
+
+    result = solution(["TR", "RT", "TR"], [7, 1, 3])
+    print("RCJA" == result, result)
